778796a88708679b79f06f76a04ba8596849bfa2
GuilhermeThome <143565577+GuilhermeThome@users.noreply.github.com>
2024-06-14 18:49:23 -0300

    Update userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,27 +1,27 @@
 import { Request, Response } from 'express';
-import * as userModel from '../models/userModel';
+import * as userService from '../services/userService';
 
 export const getAllUsers = async (req: Request, res: Response) => {
-  const users = await userModel.getUsers();
+  const users = await userService.getAllUsers();
   res.json(users);
 };
 
 export const getUser = async (req: Request, res: Response) => {
-  const user = await userModel.getUserById(Number(req.params.id));
+  const user = await userService.getUserById(Number(req.params.id));
   res.json(user);
 };
 
 export const createUser = async (req: Request, res: Response) => {
-  const newUser = await userModel.createUser(req.body);
+  const newUser = await userService.createUser(req.body);
   res.json(newUser);
 };
 
 export const updateUser = async (req: Request, res: Response) => {
-  const updatedUser = await userModel.updateUser(Number(req.params.id), req.body);
+  const updatedUser = await userService.updateUser(Number(req.params.id), req.body);
   res.json(updatedUser);
 };
 
 export const deleteUser = async (req: Request, res: Response) => {
-  await userModel.deleteUser(Number(req.params.id));
+  await userService.deleteUser(Number(req.params.id));
   res.status(204).end();
 };
