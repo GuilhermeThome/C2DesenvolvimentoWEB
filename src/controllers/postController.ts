0a44f6633c574ac81560f0116c49ed299e47a507
GuilhermeThome <143565577+GuilhermeThome@users.noreply.github.com>
2024-06-14 18:49:36 -0300

    Update postController.ts

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,27 +1,27 @@
 import { Request, Response } from 'express';
-import * as postModel from '../models/postModel';
+import * as postService from '../services/postService';
 
 export const getAllPosts = async (req: Request, res: Response) => {
-  const posts = await postModel.getPosts();
+  const posts = await postService.getAllPosts();
   res.json(posts);
 };
 
 export const getPost = async (req: Request, res: Response) => {
-  const post = await postModel.getPostById(Number(req.params.id));
+  const post = await postService.getPostById(Number(req.params.id));
   res.json(post);
 };
 
 export const createPost = async (req: Request, res: Response) => {
-  const newPost = await postModel.createPost(req.body);
+  const newPost = await postService.createPost(req.body);
   res.json(newPost);
 };
 
 export const updatePost = async (req: Request, res: Response) => {
-  const updatedPost = await postModel.updatePost(Number(req.params.id), req.body);
+  const updatedPost = await postService.updatePost(Number(req.params.id), req.body);
   res.json(updatedPost);
 };
 
 export const deletePost = async (req: Request, res: Response) => {
-  await postModel.deletePost(Number(req.params.id));
+  await postService.deletePost(Number(req.params.id));
   res.status(204).end();
 };
