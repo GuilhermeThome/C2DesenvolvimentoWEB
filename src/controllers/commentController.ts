fbfcfab885780add074c45c92928fd31a1b370dd
GuilhermeThome <143565577+GuilhermeThome@users.noreply.github.com>
2024-06-14 18:49:53 -0300

    Update commentController.ts

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -1,27 +1,27 @@
 import { Request, Response } from 'express';
-import * as commentModel from '../models/commentModel';
+import * as commentService from '../services/commentService';
 
 export const getAllComments = async (req: Request, res: Response) => {
-  const comments = await commentModel.getComments();
+  const comments = await commentService.getAllComments();
   res.json(comments);
 };
 
 export const getComment = async (req: Request, res: Response) => {
-  const comment = await commentModel.getCommentById(Number(req.params.id));
+  const comment = await commentService.getCommentById(Number(req.params.id));
   res.json(comment);
 };
 
 export const createComment = async (req: Request, res: Response) => {
-  const newComment = await commentModel.createComment(req.body);
+  const newComment = await commentService.createComment(req.body);
   res.json(newComment);
 };
 
 export const updateComment = async (req: Request, res: Response) => {
-  const updatedComment = await commentModel.updateComment(Number(req.params.id), req.body);
+  const updatedComment = await commentService.updateComment(Number(req.params.id), req.body);
   res.json(updatedComment);
 };
 
 export const deleteComment = async (req: Request, res: Response) => {
-  await commentModel.deleteComment(Number(req.params.id));
+  await commentService.deleteComment(Number(req.params.id));
   res.status(204).end();
 };
