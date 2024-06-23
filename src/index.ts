320ed80d5f249b392523e22701f2801a739ad336
GuilhermeThome <143565577+GuilhermeThome@users.noreply.github.com>
2024-06-23 12:27:35 -0300

    Update index.ts C3

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import { PrismaClient } from '@prisma/client';
 import userRouter from './routers/userRouter';
 import postRouter from './routers/postRouter';
 import commentRouter from './routers/commentRouter';
+import authRouter from './routers/authRouter';
+import { authMiddleware } from './middleware/authMiddleware';
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use('/auth', authRouter);
 app.use('/users', userRouter);
-app.use('/posts', postRouter);
-app.use('/comments', commentRouter);
+app.use('/posts', authMiddleware, postRouter);
+app.use('/comments', authMiddleware, commentRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
