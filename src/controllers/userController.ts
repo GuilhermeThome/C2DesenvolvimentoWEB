0524a08cb3544eb81096d3c0c4cce84375a65a98
GuilhermeThome <143565577+GuilhermeThome@users.noreply.github.com>
2024-06-23 12:26:01 -0300

    Update userController.ts C3

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import * as userService from '../services/userService';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET, JWT_EXPIRATION } from '../config';
 
 export const getAllUsers = async (req: Request, res: Response) => {
   const users = await userService.getAllUsers();
@@ -13,7 +15,8 @@ export const getUser = async (req: Request, res: Response) => {
 
 export const createUser = async (req: Request, res: Response) => {
   const newUser = await userService.createUser(req.body);
-  res.json(newUser);
+  const token = jwt.sign({ userId: newUser.id }, JWT_SECRET, { expiresIn: JWT_EXPIRATION });
+  res.json({ token, user: newUser });
 };
 
 export const updateUser = async (req: Request, res: Response) => {
@@ -25,3 +28,4 @@ export const deleteUser = async (req: Request, res: Response) => {
   await userService.deleteUser(Number(req.params.id));
   res.status(204).end();
 };
+
