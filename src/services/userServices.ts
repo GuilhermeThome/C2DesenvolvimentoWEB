8ab573e8e970c84c30cf3c35081bbfa54e210bbd
GuilhermeThome <143565577+GuilhermeThome@users.noreply.github.com>
2024-06-23 12:20:04 -0300

    Update C3 userServices.ts

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import * as userModel from '../models/userModel';
 
 export const getAllUsers = async () => {
@@ -8,14 +9,22 @@ export const getUserById = async (id: number) => {
   return await userModel.getUserById(id);
 };
 
-export const createUser = async (data: { email: string; name?: string }) => {
-  return await userModel.createUser(data);
+export const createUser = async (data: { email: string; name?: string; password: string }) => {
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+  return await userModel.createUser({ ...data, password: hashedPassword });
 };
 
-export const updateUser = async (id: number, data: { email?: string; name?: string }) => {
+export const updateUser = async (id: number, data: { email?: string; name?: string; password?: string }) => {
+  if (data.password) {
+    data.password = await bcrypt.hash(data.password, 10);
+  }
   return await userModel.updateUser(id, data);
 };
 
 export const deleteUser = async (id: number) => {
   return await userModel.deleteUser(id);
+};
+
+export const getUserByEmail = async (email: string) => {
+  return await userModel.getUserByEmail(email);
 };
